7f554367746c54511e1d92e022ac64678c281ae4
JackSoer <kirillrotko1@gmail.com>
2023-12-08 15:08:10 +0200

    added errors handling

--- src/BackupManager.php
+++ src/BackupManager.php
@@ -9,43 +9,59 @@ class BackupManager
 {
     public static function createBackup()
     {
-        $backupFileName = 'backup_' . date('Ymd_His') . '.sql';
+        try {
+            $backupFileName = 'backup_' . date('Ymd_His') . '.sql';
 
-        self::deleteOldBackups();
+            self::deleteOldBackups();
 
-        self::executeBackup($backupFileName);
+            self::executeBackup($backupFileName);
 
-        return $backupFileName;
+            return $backupFileName;
+        } catch (\Exception $e) {
+            \Log::error('Error creating backup: ' . $e->getMessage());
+
+            return null;
+        }
     }
 
     private static function deleteOldBackups()
     {
-        $maxBackups = config('dbbackup.max_backups', 5);
+        try {
+            $maxBackups = config('dbbackup.max_backups', 5);
 
-        $backups = Storage::files('backups');
+            $backups = Storage::files('backups');
 
-        if (count($backups) >= $maxBackups) {
-            $oldestBackups = array_slice($backups, 0, count($backups) - $maxBackups + 1);
+            if (count($backups) >= $maxBackups) {
+                $oldestBackups = array_slice($backups, 0, count($backups) - $maxBackups + 1);
 
-            Storage::delete($oldestBackups);
+                Storage::delete($oldestBackups);
+            }
+        } catch (\Exception $e) {
+            \Log::error('Error deleting old backups: ' . $e->getMessage());
         }
     }
 
     private static function executeBackup($backupFileName)
     {
-        $backupPath = storage_path('app/backups');
+        try {
+            $backupPath = storage_path('app/backups');
 
-        if (!file_exists($backupPath)) {
-            mkdir($backupPath, 0777, true);
+            if (!file_exists($backupPath)) {
+                mkdir($backupPath, 0777, true);
+            }
+
+            $databaseName = DB::getDatabaseName();
+            $username = config('database.connections.mysql.username');
+            $password = config('database.connections.mysql.password');
+            $host = config('database.connections.mysql.host');
+
+            $command = "mysqldump -u{$username} -p{$password} -h{$host} {$databaseName} > {$backupPath}/{$backupFileName}";
+
+            exec($command);
+        } catch (\PDOException $e) {
+            \Log::error('Database connection error during backup: ' . $e->getMessage());
+        } catch (\Exception $e) {
+            \Log::error('Error executing backup: ' . $e->getMessage());
         }
-
-        $databaseName = DB::getDatabaseName();
-        $username = config('database.connections.mysql.username');
-        $password = config('database.connections.mysql.password');
-        $host = config('database.connections.mysql.host');
-
-        $command = "mysqldump -u{$username} -p{$password} -h{$host} {$databaseName} > {$backupPath}/{$backupFileName}";
-
-        exec($command);
     }
 }
